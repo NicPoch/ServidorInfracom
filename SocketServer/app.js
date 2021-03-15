641b2cff52b9aecbef351b5de5cb0d539305feac
AladMocu <aa.molano@uniandes.edu.co>
2021-03-15 16:37:00 +0000

    added random file generation

--- SocketServer/app.js
+++ SocketServer/app.js
@@ -2,11 +2,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
+const { exec } = require("child_process");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//generacion de los archivos de relleno
+if(!fs.existsSync("./files/junk100mb.dat"))
+{
+    
+    exec("dd if=/dev/zero of=./files/junk100mb.dat  bs=100M  count=1")
+
+}
+if(!fs.existsSync("./files/junk250mb.dat"))
+{
+    exec("dd if=/dev/zero of=./files/junk250mb.dat  bs=250M  count=1")
+}
 
 app.use(logger('dev'));
 app.use(express.json());
